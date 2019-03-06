a48b64c858a4cade4446f3672ac330278d7e6042
tor4z <vwenjie@hotmail.com>
2019-03-06 20:04:14 +0800

    Init VS2008 dir

---
+++ VS2008/VS2008/comm.h
@@ -0,0 +1,4 @@
+#ifndef COMM_H_
+#define COMM_H_
+
+#endif // COMM_H_
---
+++ VS2008/VS2008/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "comm.h"
+
+
+int main()
+{
+	printf("Hello world!\n");
+	while(1) getchar();
+	return 0;
+}
